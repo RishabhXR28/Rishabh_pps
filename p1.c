439b3c304e81326f22999862fff2d3b7eec4c65f
RishabhXR28 <barotrishabh91@gmail.com>
2026-02-08 13:23:30 +0530

    p1.c
    Implement basic arithmetic operations with two integers.

---
+++ p1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()// Arithmetic Operations Practical
+{
+    int a=100;
+    int b=60;
+    
+    printf("The Sum of two numbers is %d\n", a+b);
+    printf("The Subtraction of two numbers is %d\n", a-b);
+    printf("The Multiplication of two numbers is %d\n", a*b);
+    printf("The Division of two numbers is %d\n", a/b);
+    printf("The Modulous of two numbers is %d\n", a%b);
+    return 0;
+}
+/* The Sum of two numbers is 160
+The Subtraction of two numbers is 40
+The Multiplication of two numbers is 6000
+The Division of two numbers is 1
+The Modulous of two numbers is 40 */
