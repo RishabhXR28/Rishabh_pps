fc0232fafc877d0095c5e9c2167d47daffc60450
RishabhXR28 <barotrishabh91@gmail.com>
2026-02-09 11:56:25 +0530

    p1.c

--- p1.c
+++ p1.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main()// Arithmetic Operations Practical
 {
-    int a=100;
-    int b=60;
+    int a, b;
+    printf("Please enter two numbers\n");
+    scanf("%d %d",&a, &b);
     
     printf("The Sum of two numbers is %d\n", a+b);
     printf("The Subtraction of two numbers is %d\n", a-b);
     printf("The Multiplication of two numbers is %d\n", a*b);
     printf("The Division of two numbers is %d\n", a/b);
-    printf("The Modulous of two numbers is %d\n", a%b);
+    printf("The Modulous of two numbers is %f\n", a%b);
     return 0;
 }
-/* The Sum of two numbers is 160
-The Subtraction of two numbers is 40
-The Multiplication of two numbers is 6000
-The Division of two numbers is 1
-The Modulous of two numbers is 40 */
+/* The Sum of two numbers is 
+The Subtraction of two numbers is 
+The Multiplication of two numbers is
+The Division of two numbers is 
+The Modulous of two numbers is  */
