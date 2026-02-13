d1ee25d3d96d3937160a7d60812ec12e609734c3
RishabhXR28 <barotrishabh91@gmail.com>
2026-02-13 13:45:27 +0530

    Update Billing System.c

--- Billing System.c
+++ Billing System.c
@@ -7,8 +7,8 @@ int main(void)
     int Book=250;
     int Butter=150;
     int a,b,c,d,e;
-    printf("\n ************************* Billing System ************************************");
     printf("\n Pen = %d\n Geometry_Box = %d \n Red_Bull = %d \n Book = %d \n Butter = %d\n ", Pen, Geometry_Box, Red_Bull, Book, Butter);
+    printf("\n ************************* Billing System ************************************\n");
     printf("Enter How Many Pen do you want\n");
     scanf("%d", &a);
     printf("Enter How Many Geometry_Box do you want\n");
@@ -22,16 +22,32 @@ int main(void)
     int x;
     x = (Pen*a) + (Geometry_Box*b) + (Red_Bull*c) + (Book*d) + (Butter*e);
     printf("Total is %d\n", x);
-    if(x>=10000)
+    if(x > 20000)
     {
-        printf("The discounted bill is %d\n", x-x*10/100);
+        printf("Discount Percentage : 12%%\n");
+        printf("Discount Amount     : %d\n", x*12/100);
+        printf("The discounted bill is %d\n", x - x*12/100);
     }
-    else if (x>=5000)
+    else if(x >= 15001)
     {
-        printf("The discounted bill is %d\n", x-x*5/100);
+        printf("Discount Percentage : 9%%\n");
+        printf("Discount Amount     : %d\n", x*9/100);
+        printf("The discounted bill is %d\n", x - x*9/100);
+    }
+    else if(x >= 10001)
+    {
+        printf("Discount Percentage : 7%%\n");
+        printf("Discount Amount     : %d\n", x*7/100);
+        printf("The discounted bill is %d\n", x - x*7/100);
+    }
+    else if(x >= 1001)
+    {
+        printf("Discount Percentage : 5%%\n");
+        printf("Discount Amount     : %d\n", x*5/100);
+        printf("The discounted bill is %d\n", x - x*5/100);
     }
     else
     {
-        printf("No discount. Total bill is %d\n",x);
+        printf("No discount. Total bill is %d\n", x);
     }
 }
